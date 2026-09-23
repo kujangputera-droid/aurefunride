7f6c79af005ebbc9fed01b10715c21716ce498bf
kujangputera-droid <kujangputera@gmail.com>
2026-09-23 21:54:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // AUREVISTA FUN RIDE 2026
-// Ganti URL di bawah ini dengan link halaman pendaftaran.
+// Isi link pendaftaran di bawah ini saat sudah tersedia.
 
 const REGISTRATION_URL = "ISI-LINK-PENDAFTARAN-DI-SINI";
 
@@ -9,7 +9,6 @@ document.querySelectorAll("[data-register]").forEach((link) => {
   link.addEventListener("click", (event) => {
     if (REGISTRATION_URL.includes("ISI-LINK")) {
       event.preventDefault();
-
       alert(
         "Link pendaftaran belum diisi. Buka script.js lalu ganti REGISTRATION_URL."
       );
